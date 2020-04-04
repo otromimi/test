6bf92744a19b8165985029329a3c9c9f553738d9
Liam <otromimi.gl@gmail.com>
2020-04-04 03:13:40 +0200

    square side to side

--- test.py
+++ test.py
@@ -1,8 +1,56 @@
-# peter la anguila
+import pygame
 
-# import request
-import numpy as nu
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("First game")
 
 
-print("caracola")
-print("como estan ustedes")
+x = 50
+y = 50
+width = 60
+height = 40
+vel = 5
+
+bounce = True
+bounce_counter = 0
+
+run = True
+
+while run:
+    pygame.time.delay(10)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    if x < (500 - width) and bounce:
+        x += vel
+    else:
+        bounce = False
+    if x != 0 and not bounce:
+        x -= vel
+    else:
+        bounce = True
+
+    if 500 - width == x:
+        bounce_counter += 1
+        print(bounce_counter)
+
+    win.fill((0, 0, 0))
+
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
